26d027e08d632600c728dca56efc4df115ffaaff
Bagaskaramadhan <bagaskaramadhan@gmail.com>
2020-09-26 02:39:36 +0700

    Progress Form Register

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,14 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@font-face {
+  font-family: 'POPPINS';
+  src: url('./assets/Poppins-ExtraBold.ttf');
+}
+* {
+  font-family: 'POPPINS';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <!-- Material form register -->
+  <form>
+    <p class="h4 text-center mb-4">Sign up</p>
+    <div class="grey-text">
+      <mdb-input label="Your name" icon="user" type="text"/>
+      <mdb-input label="Your email" icon="envelope" type="email"/>
+      <mdb-input label="Confirm your email" icon="exclamation-triangle" type="text"/>
+      <mdb-input label="Your password" icon="lock" type="password"/>
+    </div>
+    <div class="text-center">
+      <mdb-btn color="primary">Register</mdb-btn>
+    </div>
+  </form>
+  <!-- Material form register -->
 </template>
+<script>
+import { mdbInput, mdbBtn } from 'mdbvue'
+export default {
+  name: 'Basic',
+  components: {
+    mdbInput,
+    mdbBtn
+  }
+}
+</script>
