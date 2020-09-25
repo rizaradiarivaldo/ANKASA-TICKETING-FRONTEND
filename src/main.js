fa1c2d478a687e91958cb087017919fe9dda84d0
Bagaskaramadhan <bagaskaramadhan@gmail.com>
2020-09-25 21:10:47 +0700

    first FE-Ankara

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// import axios from 'axios'
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
